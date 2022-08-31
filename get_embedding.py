e43a890693a183ebf2b1ab556c0c4ff45292ba04
Anastasiya Kulikova <anastasiyakulikova@Anastasiyas-MacBook-Pro.local>
2022-08-31 13:08:10 -0400

    reorganized files

--- get_embedding.py
+++
@@ -1,5 +0,0 @@
-from bio_embeddings.embed import SeqVecEmbedder
-
-embedder = SeqVecEmbedder()
-embedding = embedder.embed("MRELDREELNCVGGAGDPLADPNSQIVRQIMSNAAWGAAFGARGGLGGMAVGAAGGVTQTVLQGAAAHMPVNVPIPKVPMGPSWNGSKG")
-print(embedding)
